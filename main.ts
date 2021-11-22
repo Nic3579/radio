538a92860212f3da13ff021abd589a77144da000
Nic3579 <92817235+Nic3579@users.noreply.github.com>
2021-11-22 14:12:50 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    encode = randint(2, 10)
-    secret_number = 40 * encode
+    secret_number = 4
     radio.sendValue("value", secret_number)
-    radio.sendValue("code", encode)
 })
 radio.onReceivedValue(function (name, value) {
     basic.showNumber(value)
 })
 let secret_number = 0
-let encode = 0
 radio.setGroup(40)
